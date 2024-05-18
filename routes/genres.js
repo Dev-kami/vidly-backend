aaf0cf718cadaa6221f168041ac659ea80772ca5
Obasi Chizitere <blossomobasi2@gmail.com>
2024-05-18 14:52:46 +0100

    Protect genres route

--- routes/genres.js
+++ routes/genres.js
@@ -1,3 +1,4 @@
+const auth = require("../middleware/auth");
 const express = require("express");
 const { Genre, validate } = require("../models/genres");
 const router = express.Router();
@@ -19,7 +20,7 @@ router.get("/:id", async (req, res) => {
 });
 
 // Add a genre
-router.post("/", async (req, res) => {
+router.post("/", auth, async (req, res) => {
   // Input validation
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.message);
@@ -33,7 +34,7 @@ router.post("/", async (req, res) => {
 });
 
 // Update a genre
-router.put("/:id", async (req, res) => {
+router.put("/:id", auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.message);
 
@@ -49,7 +50,7 @@ router.put("/:id", async (req, res) => {
 });
 
 // Delete a genre
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", auth, async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
   if (!genre)
     return res.status(404).send("The genre with the given ID was not found");
