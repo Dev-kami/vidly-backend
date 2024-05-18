cc57243a592d25784cca8033b718cb1995e1cde2
Obasi Chizitere <blossomobasi2@gmail.com>
2024-05-18 16:25:27 +0100

    Forbid users from deleting a genre if the user is not an admin

--- routes/genres.js
+++ routes/genres.js
@@ -1,3 +1,4 @@
+const admin = require("../middleware/admin");
 const auth = require("../middleware/auth");
 const express = require("express");
 const { Genre, validate } = require("../models/genres");
@@ -50,7 +51,7 @@ router.put("/:id", auth, async (req, res) => {
 });
 
 // Delete a genre
-router.delete("/:id", auth, async (req, res) => {
+router.delete("/:id", [auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
   if (!genre)
     return res.status(404).send("The genre with the given ID was not found");
