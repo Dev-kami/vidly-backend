18199b6133ce06659c57e32f2763dd139c3a3d4e
Obasi Chizitere <blossomobasi2@gmail.com>
2024-05-31 14:48:29 +0100

    feat: Update user authentication response to include user details

--- routes/auth.js
+++ routes/auth.js
@@ -16,7 +16,7 @@ router.post("/", async (req, res) => {
   if (!validPassword) return res.status(400).send("Invalid email or password.");
 
   const token = user.generateAuthToken();
-  res.send(token);
+  res.json({ token: token, user: _.pick(user, ["_id", "name", "email"]) });
 });
 
 function validate(req) {
