110a2ba9a0e110243115bbab49005358f1fe7218
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-01 11:03:22 +0100

    feat: Set default value for isAdmin field in user model and add route to get a particular

--- routes/users.js
+++ routes/users.js
@@ -16,6 +16,13 @@ router.get("/", async (req, res) => {
   res.send(users);
 });
 
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password");
+  if (!user)
+    return res.status(404).send("The user with the given ID was not found");
+  res.send(user);
+});
+
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.message);
